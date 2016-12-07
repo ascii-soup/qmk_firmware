de798e03bf02d68cbbe6a7ae4731d3e53bc30b04
Nils Luxton <ascii.soup@gmail.com>
2016-12-07 21:42:26 +0000

    Fixed LALT/LGUI mixup

--- keyboards/kc60/keymaps/asciisoup/keymap.c
+++ keyboards/kc60/keymaps/asciisoup/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  \
       KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   \
       KC_LSFT,  KC_FN0,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  \
-      KC_LCTL,  KC_LALT,  KC_LGUI,                      KC_SPC,                                 KC_INS,   KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RIGHT  \
+      KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_INS,   KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RIGHT  \
   ),
 
   /* Layer 0 (Fn0) - navigation keys
